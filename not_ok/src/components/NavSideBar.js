ab6f1786371a35dceeb5e7758ecb45aa607868bd
alicialowe <hohpascal@outlook.com>
2022-07-07 20:57:51 +0100

    removed NavSideBar module css and put into App.css. tweaked links and styling for nav bar. tweaked styling for quote so now displays in opacity box with border.

--- not_ok/src/components/NavSideBar.js
+++ not_ok/src/components/NavSideBar.js
@@ -1,18 +1,21 @@
 import React from "react";
-import navStyles from "./NavSideBar.module.css";
 
-
-//create NavSideBar function that renders the navigation bar for the side of some of the pages 
+//create NavSideBar function that renders the navigation bar for the side of some of the pages
 function NavSideBar() {
-    return (
-        <div className={navStyles.side}>
-            <a href="/write-out">I'd like to get something out</a>
-            <a href="/happy-dose">I'd like a little dose of happiness</a>
-            <a href="/tools">I'd like some suggestions for tools to help me deal with how I'm feeling</a>
-            <a href="/right-now-contacts">I think I need more help and might need to speak to someone right now</a>
+  return (
+    <div className="nav-bar">
+        <div className="nav-list">
+            <li className="nav-list-item"><a href="/write-out" className="nav-link">I'd like to get something out</a></li>
+            <li className="nav-list-item"><a href="/happy-dose" className="nav-link">I'd like a little dose of happiness</a></li>
+            <li className="nav-list-item"><a href="/tools" className="nav-link">
+                I'd like some suggestions for tools to help me deal with how I'm feeling
+            </a>
+            <li className="nav-list-item"></li><a href="/right-now-contacts" className="nav-link">
+                I think I need more help and might need to speak to someone right now
+            </a></li>
         </div>
-    )
+    </div>
+  );
 }
-
 
 export default NavSideBar;
