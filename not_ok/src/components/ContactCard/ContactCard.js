eb024e3dd2f5276d98a0ff08818a39b5dda38c00
alicialowe <hohpascal@outlook.com>
2022-07-30 14:55:48 +0100

    added FETCH to RightNowContactsPage and passed down state to map through on ContactCard component. tested in browser and data is coming through and rendering. woo!

--- not_ok/src/components/ContactCard/ContactCard.js
+++ not_ok/src/components/ContactCard/ContactCard.js
@@ -1,21 +1,21 @@
 import styles from "./ContactCard.module.css";
 
-function ContactCard() {
+function ContactCard({contacts}) {
    
     return (
-        // contacts.map((item)=>{
-        //     return (
+        contacts.map((item)=>{
+            return (
             <div className={styles.container}>
                 <div className={styles.imagecontainer}>
-                    <img src="https://cdn.shopify.com/s/files/1/0029/2401/9758/files/samaritans-logo-500px.png?height=628&pad_color=fff&v=1648718481&width=1200" alt="" />
+                    <img src={`${item.logo}`} alt="logo" />
                 </div>
                 <div className={styles.toolcontent}>
                     <div className={styles.tool_title}>
-                        <h3 className={styles.toolh3}>Mind</h3>
+                        <h3 className={styles.toolh3}>{item.name}</h3>
                     </div>
                     <div className="tool-body">
-                        <p className={styles.toolp}><i>About: </i>"We can all face problems that seem complicated or intimidating. At Citizens Advice we believe no one should have to face these problems without good quality, independent advice...Our network of independent charities offers confidential advice online, over the phone, and in person, for free. When we say we're for everyone, we mean it. People rely on us because we're independent and totally impartial."</p>
-                        <a className={styles.link} href="https://www.mind.org.uk/" target="_blank" rel="noreferrer">
+                        <p className={styles.toolp}><i>About: </i>{item.info}</p>
+                        <a className={styles.link} href={item.contact} target="_blank" rel="noreferrer">
                             Get in contact
                         </a>
                     </div>
@@ -23,8 +23,8 @@ function ContactCard() {
             </div>
             )
         }
-//     )
-//     )
-// };
+    )
+    )
+};
 
 export default ContactCard;
--- not_ok/src/pages/RightNowContactsPage.js
+++ not_ok/src/pages/RightNowContactsPage.js
@@ -1,38 +1,33 @@
 import React, {useState, useEffect} from "react";
 import ContactCard from "../components/ContactCard/ContactCard";
 
-//imports for images
-// import happyfeeds from "../assets/happydose_meme.JPG";
-
-
 import styles from "./RightNowContactsPage.module.css";
-//import ToolCard from "../components/ToolCard/ToolCard.js";
 
 
 function RightNowContactsPage() {
-    // const [tools, setTools] = useState([]);
+    const [contacts, setContacts] = useState([]);
 
-    // //request to backend for tools
-    // async function fetchTools(){
-    //     let res= await fetch('http://localhost:3001/tools')
-    //     let data = await res.json()
-    //     console.log(data.payload)
-    //     setTools(data.payload)
-    //     }
+    //request to backend for tools
+    async function fetchContacts(){
+        let res= await fetch('http://localhost:3001/rightnowcontacts')
+        let data = await res.json()
+        console.log(data.payload)
+        setContacts(data.payload)
+        }
         
-    //     //calls on page render
-    //     useEffect(()=>{
-    //       fetchTools()
-    //     },[])
+        //calls on page render
+        useEffect(()=>{
+          fetchContacts()
+        },[])
         
 
     return (
         <div className={styles.container}>
             <header><h1><a href="/">iAm != "ok"</a></h1></header>
-                <h2>Getting help is smart and brave.<br></br>Please see if someone here could help you, and if not, please reach out and look for other resources.<br></br>You matter. </h2>
+                <h2>Getting help is smart and brave.<br></br>Please see if one of these organisations could help you, and if not, please reach out and look for other resources.<br></br>You matter.</h2>
                 
                 <div className={styles.tool_selection}>
-                    <ContactCard />
+                    <ContactCard contacts={contacts}/>
                 </div>        
     
         </div>
